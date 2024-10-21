8fb357c3b14b1f3b3e1c1f0bc7bf1129ec103b3f
unknown <omesh.ch0151@gmail.com>
2024-10-21 20:39:18 +0530

    pussing task

---
+++ data_types.js
@@ -0,0 +1,33 @@
+var a = "This Is My Farst task of daTa typeS"
+console.log(a.toUpperCase(),"[USING A TOUPPERCASE TYPES]") //using a uppercase types
+console.log(typeof(a))
+let b = "RATAN NAVAL TATA 28 DECEMBER 1937,9 OCTOBER 2024 (AGE 86)"//using a tolowercase types
+console.log(b.toLowerCase(),"[USING A TOLOWERCASE TYPES]")
+console.log(b.length,"[LENGTH]")
+var a = 9160939180
+console.log(typeof(a))
+console.log(a,"[USING NUMBER TYPE]")
+const c = 3.333333
+console.log(typeof(c))
+let x = "omesh".includes("o")
+let y = "omesh".includes("oiuh")
+console.log(x,"[TRUE BOOLEAN TYPE]")
+console.log(y,"[FALSE BOOLEAN TYPE]")
+console.log(typeof(x))
+var d = undefined
+console.log(typeof(d))
+var e = null
+console.log(typeof(e))
+
+// non primetive
+
+let arr = [1,2,3,4,5,6,7]
+console.log(arr)
+console.log(typeof(arr))
+
+let obj={
+    name:"omesh",
+    city:"kurnool"
+}
+console.log(obj)
+console.log(typeof(obj))
